d0948ac29654f9e8c0520f7865f8498ef5747375
Jamie-PROG <jamiefiskerjensenok@gmail.com>
2024-09-11 10:04:59 +0200

    Update baddies money farm.lua

--- baddies money farm.lua
+++ baddies money farm.lua
@@ -1,7 +1,7 @@
 getgenv().config = {
     fps = 60,
-    gui = false,
-    checkPlayersNearby = true, -- will go away if someone is nearby (leave true if you want to be more stealthy)
+    gui = true,
+    checkPlayersNearby = false, -- will go away if someone is nearby (leave true if you want to be more stealthy)
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/db9befd2b15e75158d05001e9a430dc2.lua"))()
