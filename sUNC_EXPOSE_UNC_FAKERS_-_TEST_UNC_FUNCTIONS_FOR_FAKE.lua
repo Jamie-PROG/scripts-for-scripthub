d3d68b03091b04144d60a98e5aea9c9dfd8fd485
Jamie-PROG <jamiefiskerjensenok@gmail.com>
2024-09-14 11:01:42 +0200

    Add Lua script

---
+++ sUNC_EXPOSE_UNC_FAKERS_-_TEST_UNC_FUNCTIONS_FOR_FAKE.lua
@@ -0,0 +1,7 @@
+--[[
+   It would be ironic if your executor couldn't exec this :)
+   (Execute the source by copy pasting the raw in case of that)
+]]
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HummingBird8/HummingRn/main/sUNCTestGET"))()
